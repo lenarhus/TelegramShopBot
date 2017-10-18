da917a53e6230960aa3b42c1a30751ea733f9adf
NaveenDK <naveenkolambage@gmail.com>
2017-10-19 12:48:44 +1300

    renders the title -App Component

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React,{ Component } from 'react';
+
+class App extends Component{
+    render() {
+        return (
+            <div>
+                      <h2>Your mental shortcuts </h2>
+            </div>
+        )
+    }
+}
+
+export default App;
