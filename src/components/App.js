0a7f4fc4d570a761ea765d89f612ddfeb3e54350
NaveenDK <naveenkolambage@gmail.com>
2017-10-20 07:44:43 +1300

    seven test

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ class App extends Component{
     render() {
         return (
             <div>
-                      <h2>Your mental test this for CI sixth time</h2>
+                      <h2>Your mental test this for CI seventh time</h2>
             </div>
         )
     }
