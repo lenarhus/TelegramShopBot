55b71f8efb15650608b0a5aba3fa3580c73e5520
NaveenDK <naveenkolambage@gmail.com>
2017-10-19 17:12:56 +1300

    Ci 2

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ class App extends Component{
     render() {
         return (
             <div>
-                      <h2>Your mental test this for CI</h2>
+                      <h2>Your mental test this for CI second time</h2>
             </div>
         )
     }
