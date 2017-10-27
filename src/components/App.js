de9709e9099e9ccdba50a3e75b34b260c29534fa
NaveenDK <naveenkolambage@gmail.com>
2017-10-27 15:00:46 +1300

    b7 change

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ class App extends Component{
     render() {
         return (
             <div>
-                      <h2>This title should be shown in the deployed app when you push the code to your github repo- 21st Time we are at component did mount!</h2>
+                      <h2> we are at component did mount!..This title should be shown in the deployed app when you push the code to your github repo- 21st Time !</h2>
                       <hr/>
                       <ShortcutList/>
             </div>
