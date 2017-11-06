73d6e3c133725e02f6a43ce503e7e5f4e9c06186
NaveenDK <naveenkolambage@gmail.com>
2017-11-06 18:32:19 +1300

    test02

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ class App extends Component{
     render() {
         return (
             <div>
-improve-loading-shortcuts
+  
                       <h2>Mental Shortcuts</h2>
                       <hr/>
                       <ShortcutList/>
