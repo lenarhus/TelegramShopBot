4e7e9ed51921932418e4b0606ec936e345c5a44d
NaveenDK <naveenkolambage@gmail.com>
2017-11-07 15:34:28 +1300

    added Footer.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Footer from './Footer';
 
 
 
+
 class App extends Component{
     render() {
         return (
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,13 @@
-import React,{Component} from 'react'
+import React, {Component } from 'react'
 
 class Footer extends Component{
-render(){
-    return(
-        <div>
-            <h4> Mental Shortcuts are simple and efficient rules we can use to form judgements and make decisions </h4>
-        </div>
-    )
-
-}
-
+    render(){
+        return(
+            <div>
+                <h4>Mental Shortcuts are simple rules we can use to form judgements and make decisions</h4>
+                </div>
+        )
+    }
 }
 
 export default Footer;
